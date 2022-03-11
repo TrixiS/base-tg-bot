0fe03c82defc3424a4afbf654476cbcfb573f46d
TrixiS <oficialmorozov@gmail.com>
2022-03-11 22:26:01 +0500

    Set html as default parse mode

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from aiogram import Dispatcher
+from aiogram import Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from .config import BotConfig
@@ -10,5 +10,8 @@ from .utils.bot import Bot
 config = BotConfig.load_first()
 all_phrases = BotPhrases.load_all()
 
-bot = Bot(config, all_phrases, loop=asyncio.get_event_loop())
+bot = Bot(
+    config, all_phrases, loop=asyncio.get_event_loop(), parse_mode=types.ParseMode.HTML
+)
+
 dispatcher = Dispatcher(bot, loop=bot.loop, storage=MemoryStorage())
