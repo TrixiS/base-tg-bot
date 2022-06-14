7b6b0e47480cf2c7ba9ba874afe2844ab0ca6ee9
Andrei Morozov <oficialmorozov@gmail.com>
2022-06-15 02:22:08 +0500

    Update start.py

--- bot/routers/user/start.py
+++ bot/routers/user/start.py
@@ -4,6 +4,6 @@ from ...bot import bot
 from . import router
 
 
-@router.message(commands=["start"])
+@router.message(commands=["start"], state=None)
 async def start_handler(message: types.Message):
     await message.answer(bot.phrases.start_message)
