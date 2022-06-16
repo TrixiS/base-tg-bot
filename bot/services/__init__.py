b9ce6ba55e1c60610addf38bd263730ff671006f
TrixiS <oficialmorozov@gmail.com>
2022-06-16 18:44:25 +0500

    Add schedule service

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -1,11 +1,15 @@
 from .database import DatabaseService
+from .schedule import ScheduleService, jobs
 
 database_service = DatabaseService()
+schedule_service = ScheduleService()
 
 
 async def setup():
     await database_service.setup()
+    await schedule_service.setup()
 
 
 async def dispose():
     await database_service.dispose()
+    await schedule_service.dispose()
