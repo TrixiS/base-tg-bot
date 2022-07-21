94694c9ed38ba1c12c5d16ecf2466df51c8d6534
TrixiS <oficialmorozov@gmail.com>
2022-07-21 19:27:07 +0500

    Assign names to the services in the services manager

--- bot/middleware/services_di.py
+++ bot/middleware/services_di.py
@@ -1,19 +1,9 @@
-import re
 from typing import Any, Awaitable, Callable, Dict
 
-from aiogram import types
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
 
 from ..protocols.telegram_user_event import TelegramUserEvent
 from ..utils.dispatcher import Dispatcher
-
-
-def snake_case(s: str):
-    return "_".join(
-        re.sub(
-            "([A-Z][a-z]+)", r" \1", re.sub("([A-Z]+)", r" \1", s.replace("-", " "))
-        ).split()
-    ).lower()
 
 
 class ServicesDIMiddleware(BaseMiddleware):
@@ -26,8 +16,5 @@ class ServicesDIMiddleware(BaseMiddleware):
         event: TelegramUserEvent,
         data: Dict[str, Any],
     ) -> Any:
-        for service in self.dispatcher.services._services:
-            service_key = snake_case(service.__class__.__name__)
-            data[service_key] = service
-
+        data.update(self.dispatcher.services._services)
         return await handler(event, data)
