e2c8a9586ddcdde804a9157bee7d2e1dea7fcba8
Andrei Morozov <oficialmorozov@gmail.com>
2022-08-23 16:32:24 +0000

    Add aerich

--- bot/services/database/__init__.py
+++ bot/services/database/__init__.py
@@ -2,14 +2,20 @@ from tortoise import Tortoise
 
 from ...bot import bot
 
+TORTOISE_ORM = {
+    "connections": {"default": bot.config.database_uri},
+    "apps": {
+        "models": {
+            "models": ["bot.services.database.models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
+
 
 class DatabaseService:
     async def setup(self):
-        await Tortoise.init(
-            modules={"models": ["bot.services.database.models"]},
-            db_url=bot.config.database_uri,
-        )
-
+        await Tortoise.init(TORTOISE_ORM)
         await Tortoise.generate_schemas()
 
     async def dispose(self):
