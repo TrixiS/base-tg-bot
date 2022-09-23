f178431d7a7cad06cbc65d75a92d369e759fd57a
Andrei Morozov <oficialmorozov@gmail.com>
2022-09-23 00:45:54 +0000

    Add default bot user filter

---
+++ bot/routers/user/test.py
@@ -0,0 +1,12 @@
+﻿from aiogram import F, types
+from aiogram.fsm.context import FSMContext
+
+from ... import filters, markups
+from ...bot import bot
+from ...services.database.models import BotUser
+from . import router
+
+
+@router.message(filters.bot_user_filter)
+async def bot_user_handler(message: types.Message, target_bot_user: BotUser):
+    await message.answer(str(target_bot_user.id))
