662780c7f97207b22384a1d8736932f78fc86561
Andrei Morozov <oficialmorozov@gmail.com>
2022-10-22 21:21:54 +0000

    Cache current field generators

---
+++ bot/routers/user/form.py
@@ -0,0 +1,23 @@
+﻿import datetime
+
+from aiogram import Dispatcher, F, types
+from aiogram.fsm.context import FSMContext
+
+from ... import markups
+from ...bot import bot
+from ...services.database.models import BotUser
+from ...utils.form import Form, FormField
+from . import router
+
+
+class TestForm(Form):
+    # photo: types.PhotoSize = FormField(enter_message_text="Enter photo")
+    document: types.Document = FormField(enter_message_text="Enter document")
+
+    async def submit(self, **data):
+        await bot.send_document(data["event_chat"].id, self.document.file_id)
+
+
+@router.message(F.text == "/form")
+async def form_handler(message: types.Message, state: FSMContext):
+    await TestForm.start(router, state)
--- bot/utils/form.py
+++ bot/utils/form.py
@@ -1,12 +1,13 @@
 import datetime
-from abc import ABC, ABCMeta
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any, Optional, Type, TypeVar
+from typing import Any, Dict, Generator, Optional, Type, TypeVar
 
 from aiogram import F, types
 from aiogram.dispatcher.router import Router
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.base import StorageKey
 from aiogram.utils.magic_filter import MagicFilter
 
 # TODO: support for pattern enter message like
@@ -24,7 +25,7 @@ class FormState(StatesGroup):
     waiting_field_value = State()
 
 
-@dataclass
+@dataclass(frozen=True)
 class FormFieldInfo:
     enter_message_text: str
     filter: Optional[MagicFilter]
@@ -41,9 +42,36 @@ class _FormFieldData:
     info: FormFieldInfo
 
 
-class Form(ABC, metaclass=ABCMeta):
+class Form(ABC):
+    __field_generator_cache: Dict[
+        StorageKey, Generator[_FormFieldData, None, None]
+    ] = {}
+
+    __default_filters = {
+        str: F.text,
+        int: F.text.func(int),
+        float: F.text.func(float),
+        datetime.date: F.text.func(
+            lambda text: datetime.datetime.strptime(text, r"%d.%m.%Y").date()
+        ),
+        datetime.datetime: F.text.func(
+            lambda text: datetime.datetime.strptime(text, r"%d.%m.%Y %H:%M")
+        ),
+        datetime.time: F.text.func(
+            lambda text: datetime.datetime.strptime(text, r"%H:%M").time()
+        ),
+        types.PhotoSize: F.photo.func(lambda photo: photo[-1]),
+        types.Document: F.document.func(lambda document: document),
+    }
+
     @classmethod
-    def __current_field_generator(cls):
+    def __from_state_data(cls, state_data):
+        form_object = cls()
+        form_object.__dict__.update(state_data["values"])
+        return form_object
+
+    @classmethod
+    def __create_current_field_generator(cls):
         for field_name, field_type in cls.__annotations__.items():
             field_info = getattr(cls, field_name)
 
@@ -54,23 +82,25 @@ class Form(ABC, metaclass=ABCMeta):
             yield field_data
 
     @classmethod
-    def __get_filter_from_type(cls, field_type: Type):
-        default_filters = {
-            str: F.text,
-            int: F.text.func(int),
-            float: F.text.func(float),
-            datetime.date: F.text.func(
-                lambda text: datetime.datetime.strptime(text, r"%d.%m.%Y").date()
-            ),
-            datetime.datetime: F.text.func(
-                lambda text: datetime.datetime.strptime(text, r"%d.%m.%Y %H:%M")
-            ),
-            datetime.time: F.text.func(
-                lambda text: datetime.datetime.strptime(text, r"%H:%M").time()
-            ),
-        }
+    def __get_current_field_generator(cls, key: StorageKey):
+        if generator := cls.__field_generator_cache.get(key):
+            return generator
 
-        field_filter = default_filters.get(field_type)
+        generator = cls.__create_current_field_generator()
+        cls.__field_generator_cache[key] = generator
+        return generator
+
+    @classmethod
+    def __clear_field_generator_cache(cls, key: StorageKey):
+        if key not in cls.__field_generator_cache:
+            return False
+
+        del cls.__field_generator_cache[key]
+        return True
+
+    @classmethod
+    def __get_filter_from_type(cls, field_type: Type):
+        field_filter = cls.__default_filters.get(field_type)
 
         if field_filter is None:
             raise ValueError("This field type is not supported yet")
@@ -83,10 +113,18 @@ class Form(ABC, metaclass=ABCMeta):
         router: Router,
         state_ctx: FSMContext,
     ):
-        field_generator = cls.__current_field_generator()
+        cls.__clear_field_generator_cache(state_ctx.key)
+        field_generator = cls.__get_current_field_generator(state_ctx.key)
         first_field: _FormFieldData = next(field_generator)
+
         await state_ctx.set_state(FormState.waiting_field_value)
         await state_ctx.update_data(current_field=first_field, values={})
+        await state_ctx.bot.send_message(
+            state_ctx.key.chat_id, first_field.info.enter_message_text
+        )
+
+        if getattr(cls, "__registered", False):
+            return
 
         async def resolve_callback(
             message: types.Message, state: FSMContext, value: Any, **data
@@ -96,14 +134,17 @@ class Form(ABC, metaclass=ABCMeta):
             state_data["values"][current_field.name] = value
             await state.set_data(state_data)
 
+            current_field_generator = cls.__get_current_field_generator(state.key)
+
             try:
-                next_field = next(field_generator)
+                next_field = next(current_field_generator)
                 await state.update_data(current_field=next_field)
             except StopIteration:
                 state_data = await state.get_data()
                 await state.clear()
-                form_object = cls()
-                form_object.__dict__.update(state_data["values"])
+                data["state"] = state
+                form_object = cls.__from_state_data(state_data)
+                cls.__clear_field_generator_cache(state.key)
                 return await form_object.submit(**data)
 
             await message.answer(next_field.info.enter_message_text)
@@ -123,16 +164,12 @@ class Form(ABC, metaclass=ABCMeta):
 
             return {"value": filter_result}
 
-        if not getattr(cls, "__registered", False):
-            router.message.register(
-                resolve_callback, FormState.waiting_field_value, current_field_filter
-            )
-
-            cls.__registered = True
-
-        await state_ctx.bot.send_message(
-            state_ctx.key.chat_id, first_field.info.enter_message_text
+        router.message.register(
+            resolve_callback, FormState.waiting_field_value, current_field_filter
         )
 
+        cls.__registered = True
+
+    @abstractmethod
     async def submit(self, **data):
         ...
