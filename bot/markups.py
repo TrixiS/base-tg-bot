be845a37589f552a21f06cc64a5bf66302f447d0
Andrei Morozov <oficialmorozov@gmail.com>
2022-10-31 01:21:58 +0000

    Add remove markup

--- bot/markups.py
+++ bot/markups.py
@@ -3,3 +3,5 @@ from aiogram.types import InlineKeyboardButton, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 from .bot import bot
+
+remove_markup = types.ReplyKeyboardRemove(remove_keyboard=True)
