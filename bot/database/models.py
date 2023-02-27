7397d01b208d8c8a23ed3bc96ce8b27a7011b089
TrixiS <oficialmorozov@gmail.com>
2023-02-27 13:57:00 +0300

    Separate BotUser.url and .mention

--- bot/database/models.py
+++ bot/database/models.py
@@ -8,8 +8,15 @@ class BotUser(Model):
     full_name = fields.TextField()
 
     @property
-    def link(self):
+    def mention(self):
         if self.username:
             return f"@{self.username}"
 
         return f'<a href="tg://user?id={self.id}">{self.full_name}</a>'
+
+    @property
+    def url(self):
+        if self.username:
+            return f"https://t.me/{self.username}"
+
+        return f"tg://user?id={self.id}"
