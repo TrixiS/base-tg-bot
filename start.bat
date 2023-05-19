50e1660be8ce724922da540495d494f647f65849
TrixiS <oficialmorozov@gmail.com>
2023-05-19 15:00:17 +0300

    Update start scripts

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-(python -m pip install -U wheel -r requirements.txt --quiet && python -m prisma db push && python -m bot) || pause
+(python -m pip install -U wheel -r requirements.txt --quiet && python -m bot) || pause
