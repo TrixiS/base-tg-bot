d6bab885a3504d847e0d97e508b38a07cc4c9912
TrixiS <oficialmorozov@gmail.com>
2023-06-17 14:56:52 +0300

    Make Router.filter accept aiogram CallbackType

--- bot/utils/router.py
+++ bot/utils/router.py
@@ -1,8 +1,8 @@
+from aiogram.dispatcher.event.handler import CallbackType
 from aiogram.dispatcher.router import Router as AiogramRouter
-from aiogram.filters import BaseFilter
 
 
 class Router(AiogramRouter):
-    def filter(self, bound_filter: BaseFilter) -> None:
+    def filter(self, *filters: CallbackType) -> None:
         for observer in self.observers.values():
-            observer.filter(bound_filter)
+            observer.filter(*filters)
