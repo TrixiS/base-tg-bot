ba6e8a3b1783dbe5e798de95b37047cb32da5b96
TrixiS <oficialmorozov@gmail.com>
2023-07-31 02:43:46 +0300

    Cast admin user id to int

--- bot/filters/__init__.py
+++ bot/filters/__init__.py
@@ -4,7 +4,7 @@ from ..config import config
 
 
 def is_admin(user_id: int):
-    return config.admin_user_id == user_id
+    return int(config.admin_user_id) == user_id
 
 
 async def admin_filter(telegram_object: types.TelegramObject):
