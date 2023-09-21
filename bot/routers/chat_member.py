6d09c7db63d21439c4a20e4d052bc9be631bb582
TrixiS <oficialmorozov@gmail.com>
2023-09-21 16:32:26 +0200

    Fix bot user chat member handlers

--- bot/routers/chat_member.py
+++ bot/routers/chat_member.py
@@ -1,6 +1,6 @@
 ﻿import datetime
 
-from aiogram import F, types
+from aiogram import F, enums, types
 from aiogram.filters import IS_MEMBER, IS_NOT_MEMBER, ChatMemberUpdatedFilter
 
 from ..database.models import BotChat, BotUser
@@ -11,24 +11,28 @@ router = Router()
 root_router.include_router(router)
 
 
-@router.chat_member(
-    F.chat.type == "private", ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER)
+@router.my_chat_member(
+    F.chat.type == enums.ChatType.PRIVATE,
+    ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER),
 )
 async def joined_user_handler(_: types.ChatMemberUpdated, bot_user: BotUser):
-    await BotUser.filter(id=bot_user.id).update(
-        joined_at=datetime.datetime.utcnow(), left_at=None
-    )
-
-
-@router.chat_member(
-    F.chat.type == "private", ChatMemberUpdatedFilter(IS_MEMBER >> IS_NOT_MEMBER)
-)
-async def left_user_handler(_: types.ChatMemberUpdated, bot_user: BotUser):
-    await BotUser.filter(id=bot_user.id).update(left_at=datetime.datetime.utcnow())
+    bot_user.joined_at = datetime.datetime.now(datetime.timezone.utc)
+    bot_user.left_at = None  # type: ignore
+    await bot_user.save()
 
 
 @router.my_chat_member(
-    F.chat.type != "private", ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER)
+    F.chat.type == enums.ChatType.PRIVATE,
+    ChatMemberUpdatedFilter(IS_MEMBER >> IS_NOT_MEMBER),
+)
+async def left_user_handler(_: types.ChatMemberUpdated, bot_user: BotUser):
+    bot_user.left_at = datetime.datetime.now(datetime.timezone.utc)
+    await bot_user.save()
+
+
+@router.my_chat_member(
+    F.chat.type != enums.ChatType.PRIVATE,
+    ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER),
 )
 async def new_chat_handler(update: types.ChatMemberUpdated):
     await BotChat.update_or_create(
@@ -42,7 +46,8 @@ async def new_chat_handler(update: types.ChatMemberUpdated):
 
 
 @router.my_chat_member(
-    F.chat.type != "private", ChatMemberUpdatedFilter(IS_MEMBER >> IS_NOT_MEMBER)
+    F.chat.type != enums.ChatType.PRIVATE,
+    ChatMemberUpdatedFilter(IS_MEMBER >> IS_NOT_MEMBER),
 )
 async def removed_chat_handler(update: types.ChatMemberUpdated):
     await BotChat.filter(id=update.chat.id).delete()
