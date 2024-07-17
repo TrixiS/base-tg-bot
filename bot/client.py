b68ba3ec1e65c68f5241bbd3d3042f59d7654320
TrixiS <oficialmorozov@gmail.com>
2024-07-17 22:43:22 +0300

    Bump to aiogram3.8.0

--- bot/client.py
+++ bot/client.py
@@ -1,9 +1,14 @@
 import asyncio
+import logging
 
 from aiogram import exceptions
 from aiogram.client.bot import Bot as _Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 
 from .config import config
+
+TELEGRAM_REQUEST_TIMEOUT_SECONDS = 2 * 60
 
 
 class Bot(_Bot):
@@ -13,6 +18,15 @@ class Bot(_Bot):
                 return await super().__call__(*args, **kwargs)
             except exceptions.TelegramRetryAfter as e:
                 await asyncio.sleep(e.retry_after)
+            except (
+                exceptions.TelegramNetworkError,
+                asyncio.exceptions.TimeoutError,
+            ) as e:
+                logging.error(e, exc_info=True)
 
 
-bot = Bot(token=config.bot_token, parse_mode="HTML")
+bot = Bot(
+    token=config.bot_token,
+    default=DefaultBotProperties(parse_mode="HTML"),
+    session=AiohttpSession(timeout=TELEGRAM_REQUEST_TIMEOUT_SECONDS),
+)
