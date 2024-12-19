1e4c181e2626bc82554f4a3630a6ff8eafa6d70e
TrixiS <oficialmorozov@gmail.com>
2024-12-19 13:07:26 +0300

    Remove remove_markup from markups template

--- bot/markups.py
+++ bot/markups.py
@@ -3,5 +3,3 @@ from aiogram.types import InlineKeyboardButton, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 from .phrases import phrases
-
-remove_markup = types.ReplyKeyboardRemove(remove_keyboard=True)
