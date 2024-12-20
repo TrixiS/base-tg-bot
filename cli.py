bba90f59b0f659a800020f2c260a2b67e2c5cf16
TrixiS <oficialmorozov@gmail.com>
2024-12-20 09:46:13 +0300

    Refactor cli

--- cli.py
+++ cli.py
@@ -1,25 +1,14 @@
 import os
 from pathlib import Path
-from typing import Iterable
 
 import typer
 
-from bot.config import DEV_ENV_FILEPATH, ENCODING, PROD_ENV_FILEPATH, Config
-from bot.utils.paths import ROOT_PATH, ROUTERS_PATH
+from bot import config
+from bot.utils import paths
 
 app = typer.Typer()
 
-
-@app.command()
-def update():
-    os.system("uv export --no-hashes > requirements.txt")
-    update_env_files()
-
-
-@app.command("r")
-@app.command("router")
-def router(name: str, file: bool = False, jump: bool = False):
-    FILE_ROUTER_CODE = """from aiogram import F, Router, types
+FILE_ROUTER_CODE = """from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
 
 from .. import markups
@@ -32,7 +21,7 @@ router = Router()
 root_router.include_router(router)
 """
 
-    DIR_ROUTER_CODE = """from aiogram import Router
+DIR_ROUTER_CODE = """from aiogram import Router
 
 from .. import root_router
 
@@ -40,30 +29,7 @@ router = Router()
 root_router.include_router(router)
 """
 
-    if file:
-        router_filepath = ROUTERS_PATH / f"{name}.py"
-        router_filepath.write_text(FILE_ROUTER_CODE, encoding=ENCODING)
-        typer.echo(f"Created router in {router_filepath}")
-
-        if jump:
-            jump_to_file(router_filepath)
-
-        return
-
-    router_dirpath = ROUTERS_PATH / name
-    init_filepath = router_dirpath / "__init__.py"
-    router_dirpath.mkdir(exist_ok=True)
-    init_filepath.write_text(DIR_ROUTER_CODE, encoding=ENCODING)
-    typer.echo(f"Created router in {init_filepath}")
-
-    if jump:
-        jump_to_file(init_filepath)
-
-
-@app.command("h")
-@app.command("handler")
-def handler(router: str, name: str, jump: bool = False):
-    HANDLER_CODE = """from aiogram import F, types
+HANDLER_CODE = """from aiogram import F, types
 from aiogram.fsm.context import FSMContext
 
 from ... import markups
@@ -73,7 +39,39 @@ from ...phrases import phrases
 from . import router
 """
 
-    router_dirpath = ROUTERS_PATH / router
+
+@app.command()
+def update():
+    os.system("uv export --no-hashes > requirements.txt")
+    update_env_file(config.PROD_ENV_FILEPATH)
+    update_env_file(config.DEV_ENV_FILEPATH)
+
+
+@app.command("router")
+def router(name: str, file: bool = False, jump: bool = False):
+    if file:
+        router_filepath = paths.ROUTERS_PATH / f"{name}.py"
+        router_filepath.write_text(FILE_ROUTER_CODE, encoding=config.ENCODING)
+        typer.echo(f"Created router in {router_filepath}")
+
+        if jump:
+            jump_to_file(router_filepath)
+
+        return
+
+    router_dirpath = paths.ROUTERS_PATH / name
+    init_filepath = router_dirpath / "__init__.py"
+    router_dirpath.mkdir(exist_ok=True)
+    init_filepath.write_text(DIR_ROUTER_CODE, encoding=config.ENCODING)
+    typer.echo(f"Created router in {init_filepath}")
+
+    if jump:
+        jump_to_file(init_filepath)
+
+
+@app.command("handler")
+def handler(router: str, name: str, jump: bool = False):
+    router_dirpath = paths.ROUTERS_PATH / router
 
     if not router_dirpath.exists():
         return typer.echo(f"Router {router} does not exist")
@@ -83,7 +81,7 @@ from . import router
     if handler_filepath.exists():
         typer.echo(f"Handler {handler_filepath} is already created")
     else:
-        handler_filepath.write_text(HANDLER_CODE, encoding=ENCODING)
+        handler_filepath.write_text(HANDLER_CODE, encoding=config.ENCODING)
         typer.echo(f"Handler {handler_filepath} has been created")
 
     if jump:
@@ -94,42 +92,14 @@ def jump_to_file(path: Path):
     os.system(f"code {path.absolute()}")
 
 
-def generate_filepaths(filenames: Iterable[Path]):
-    yield from map(lambda filename: ROOT_PATH / filename, filenames)
-
-
-def _settings_properties_values_generator(
-    settings_object: Config, field_names: Iterable[str]
-):
-    for field_name in field_names:
-        value = getattr(settings_object, field_name)
-        yield field_name, value
-
-
 def update_env_file(filepath: Path):
-    settings_object = Config(
-        _env_file=str(filepath.absolute()),  # type: ignore
-        _env_file_encoding=ENCODING,  # type: ignore
-    )
-
     env_file_content = "\n".join(
-        f"{field_name.upper()}={field_value}"
-        for field_name, field_value in _settings_properties_values_generator(
-            settings_object, settings_object.model_fields.keys()
-        )
+        f"{name.upper()}={value.default}"
+        for name, value in config.Config.model_fields.items()
     )
 
-    env_file_content += "\n"  # trailing new line
-
-    filepath.write_text(
-        env_file_content,
-        encoding=ENCODING,
-    )
-
-
-def update_env_files():
-    update_env_file(PROD_ENV_FILEPATH)
-    update_env_file(DEV_ENV_FILEPATH)
+    env_file_content += "\n"
+    filepath.write_text(env_file_content, encoding=config.ENCODING)
 
 
 if __name__ == "__main__":
