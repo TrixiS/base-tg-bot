87403f6250dccfe5cc3724a2261a5619e79bf617
TrixiS <oficialmorozov@gmail.com>
2025-07-19 18:05:34 +0300

    Move list markup functions to base-tg-bot-modules

--- bot/markups.py
+++ bot/markups.py
@@ -1,28 +1,5 @@
-from typing import Callable, Iterable, TypeVar
-
 from aiogram import types
-from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 from . import phrases
-
-_T = TypeVar("_T")
-
-
-def _create_list_markup_builder(
-    it: Iterable[_T], cb: Callable[[_T], tuple[str, CallbackData]]
-):
-    builder = InlineKeyboardBuilder()
-
-    for i in it:
-        text, cb_data = cb(i)
-        builder.button(text=text, callback_data=cb_data.pack())
-
-    return builder
-
-
-def create_list_markup(
-    it: Iterable[_T], cb: Callable[[_T], tuple[str, CallbackData]]
-) -> types.InlineKeyboardMarkup:
-    return _create_list_markup_builder(it, cb).adjust(1, repeat=True).as_markup()  # type: ignore
