af97edd893464776ae73224e561a59afbf4894bb
TrixiS <oficialmorozov@gmail.com>
2025-12-06 15:42:00 +0300

    Refactor BotUser

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,3 +1,5 @@
+import html
+
 from tortoise import fields
 from tortoise.models import Model
 
@@ -13,24 +15,13 @@ class BotUser(Model):
     @property
     def full_name(self) -> str:
         if self.last_name:
-            return f"{self.first_name} {self.last_name}"
+            return html.escape(f"{self.first_name} {self.last_name}")
 
-        return self.first_name
-
-    @property
-    def tg_url(self) -> str:
-        return f"tg://user?id={self.id}"
-
-    @property
-    def url(self):
-        if self.username:
-            return f"https://t.me/{self.username}"
-
-        return self.tg_url
+        return html.escape(self.first_name)
 
     @property
     def mention(self):
         if self.username:
             return f"@{self.username}"
 
-        return f'<a href="{self.tg_url}">{self.full_name}</a>'
+        return f'<a href="tg://user?id={self.id}">{self.full_name}</a>'
